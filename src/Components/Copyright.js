ee23e904b51d4744e2979576cb41f590ad8ff8c2
Kamran Ashraf <79129427+codeByKamran@users.noreply.github.com>
2021-02-16 13:21:47 +0500

    Update Copyright.js

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -5,9 +5,9 @@ const CopyrightSection = () => {
   return (
     <div className="copyright__section">
       <h1>
-        @This <strong>COVID-19 Tracker</strong> is Built by{" "}
-        <a href="https://www.azharzaman.com" target="_blank">
-          Azhar Zaman
+        @This <strong>COVID-19 Tracker</strong> is built by{" "}
+        <a href="https://kamran-covid19tracker.netlify.app/" target="_blank">
+          Kamran A.
         </a>
         with <strong>React</strong> to Showcase Skills@
       </h1>
